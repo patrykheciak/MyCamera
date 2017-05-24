8222d006444071787ff9bc0adcea079b6f2df158
patrykheciak <patrykheciak@gmail.com>
2017-05-24 19:56:05 +0200

    gallery

--- app/src/main/java/com/gmail/patrykheciak/mycamera/MainActivity.java
+++ app/src/main/java/com/gmail/patrykheciak/mycamera/MainActivity.java
@@ -2,6 +2,8 @@ package com.gmail.patrykheciak.mycamera;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,11 +11,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE_CAPTURE_IMAGE = 2137;
+    private static final int REQUEST_CODE_PHOTO_GALLERY = 1337;
     private ImageView imageView;
     private Button buttonSelfie;
+    private Button buttonGallery;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +34,61 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = (ImageView) findViewById(R.id.image);
         buttonSelfie = (Button) findViewById(R.id.selfieButton);
-
+        buttonGallery = (Button) findViewById(R.id.galleryButton);
         buttonSelfie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent capturePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (capturePhotoIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(capturePhotoIntent, REQUEST_CODE_CAPTURE_IMAGE);
-                }
+                setSelfieButton();
             }
         });
+        buttonGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setGalleryButton();
+            }
+        });
+    }
+
+    private void setGalleryButton() {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(
+                Intent.createChooser(galleryIntent, "Choose photo"), REQUEST_CODE_PHOTO_GALLERY);
+
+    }
+
+    private void setSelfieButton() {
+        Intent capturePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        capturePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, getPhotoUri());
+        if (capturePhotoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(capturePhotoIntent, REQUEST_CODE_CAPTURE_IMAGE);
+        }
+    }
+
+    private Uri getPhotoUri() {
+//        String fileName = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss", Locale.getDefault()).format(new Date());
+        File photoFile = new File(Environment.getExternalStorageDirectory(), "avatar.jpg");
+        return Uri.fromFile(photoFile);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_CODE_CAPTURE_IMAGE & resultCode == RESULT_OK) {
-            Bitmap photoBitmap = (Bitmap) data.getExtras().get("data");
-            if (photoBitmap != null) {
-                imageView.setImageBitmap(photoBitmap);
+            if (data != null && data.getExtras() != null) {
+                Bitmap photoBitmap = (Bitmap) data.getExtras().get("data");
+                if (photoBitmap != null) {
+                    imageView.setImageBitmap(photoBitmap);
+                }
+            } else {
+                imageView.setImageURI(getPhotoUri());
+                Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                intent.setData(getPhotoUri());
+                sendBroadcast(intent);
             }
+        } else if (requestCode == REQUEST_CODE_PHOTO_GALLERY && resultCode == RESULT_OK) {
+            Uri photoImage = data.getData();
+            imageView.setImageURI(photoImage);
         }
     }
 }
